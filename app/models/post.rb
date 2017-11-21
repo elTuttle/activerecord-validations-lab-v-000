e97cfe512128c8713feb00974d0b878e5eb8328c
John Tuttle <thejwt@gmail.com>
2017-11-21 07:26:34 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ActiveRecord::Base
   validate :clickbait
 
   def clickbait
-    if self.title.include?("Won't Believe")||self.title.include?("Secret")||self.title.include?("Top")||self.title.include?("Guess")
+    if self.title.include?("Won't Believe")||self.title.include?("True Facts")||self.title.include?("Secret")||self.title.include?("Top")||self.title.include?("Guess")
       true
     else
       false
